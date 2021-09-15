981004caefa79b6dd3110b7419393bd2c61874ec
Dream-prince <77263344+dreamboy-cyber@users.noreply.github.com>
2021-09-15 15:14:05 +0530

    Update test.js

--- test.js
+++ test.js
@@ -6,9 +6,7 @@ const client = new Client({
 				Intents.FLAGS.GUILDS,
 				Intents.FLAGS.GUILD_MEMBERS,
 				Intents.FLAGS.GUILD_MESSAGES,
-				Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-				Intents.FLAGS.GUILD_VOICE_STATES,
-				Intents.FLAGS.DIRECT_MESSAGES
+				
 			],
 });
 
